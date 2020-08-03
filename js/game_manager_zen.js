60fd4ed2bf72d3fded2eedf4772a1e054a51b26c
TheReal4096 <69132192+TheReal4096@users.noreply.github.com>
2020-08-03 11:09:40 -0700

    Update game_manager_zen.js

--- js/game_manager_zen.js
+++ js/game_manager_zen.js
@@ -67,10 +67,9 @@ GameManager.prototype.addStartTiles = function () {
 
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
-  if (this.grid.cellsAvailable() && this.score < Infinity) {
+  if (this.grid.cellsAvailable()) {
     var value = 2 + Math.floor(this.score / 50);
-    value = Math.min(value, Infinity - this.score);
-    var tile = new Tile(this.grid.randomAvailableCell(), value);
+   var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
     this.score += value;
